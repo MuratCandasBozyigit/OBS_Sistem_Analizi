c0f9c32ab03db102407bf2b0823b804e22386254
Murti <bozyigit31@gmail.com>
2025-05-11 17:55:32 +0300

    Tüm Login Sonrası Sayfalar Hazır DB düzenlemeye geçyiorum artık sonrasında devam ederims

--- Frames/SubFrames/Teacher/mainT.py
+++ Frames/SubFrames/Teacher/mainT.py
@@ -19,16 +19,16 @@ def teacher_gui():
     frame.grid_columnconfigure(0, weight=1)
 
     # Butonları oluşturuyoruz
-    dersler_button = ctk.CTkButton(frame, text="Eğitim Görevlisi Olduğum Dersler", width=200, height=50, command=yon1)
-    dersler_button.grid(row=0, column=0, pady=10)
+    myClasses_button = ctk.CTkButton(frame, text="Eğitim Görevlisi Olduğum Dersler", width=200, height=50, command=yon1)
+    myClasses_button.grid(row=0, column=0, pady=10)
 
-    notlar_button = ctk.CTkButton(frame, text="Tüm Öğrenciler", width=200, height=50, command=yon2)
-    notlar_button.grid(row=1, column=0, pady=10)
+    allStudents_button = ctk.CTkButton(frame, text="Tüm Öğrenciler", width=200, height=50, command=yon2)
+    allStudents_button.grid(row=1, column=0, pady=10)
 
  
 
     # Butonlara hover efektleri ekliyoruz
-    for button in [dersler_button, notlar_button, sinavlar_button]:
+    for button in [myClasses_button, allStudents_button]:
         button.bind("<Enter>", lambda e, b=button: b.configure(fg_color="darkblue"))
         button.bind("<Leave>", lambda e, b=button: b.configure(fg_color="gray"))
 
