0561d0c984a6181289cb5de01c8a52a9769e54d7
Murti <bozyigit31@gmail.com>
2025-05-13 02:08:19 +0300

    GEGE

--- Frames/__init__.py
+++ Frames/__init__.py
@@ -1,2 +1,2 @@
 from .startFrame import build_gui
-from .SubFrames import admin_gui, student_gui, teacher_gui,ders_ekleme_sayfasi
+from .SubFrames import admin_gui, student_gui, teacher_gui,ders_ekleme_sayfasi,ogrenci_ekleme_sayfasi
