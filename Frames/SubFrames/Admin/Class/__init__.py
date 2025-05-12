bc6bffaac78967b9b81610cc0fa54b5f792dbc2d
Murti <bozyigit31@gmail.com>
2025-05-12 21:08:52 +0300

    Sayfa sayısını azalttımss her şey tamam şimdi devam edeceğim.

--- Frames/SubFrames/Admin/Class/__init__.py
+++ Frames/SubFrames/Admin/Class/__init__.py
@@ -1,3 +1,2 @@
 from .createClass import ders_ekleme_sayfasi
-from .classes import dersler_yonetim_penceresi
 from .getClasses import dersleri_listele_gui
--- Frames/SubFrames/Admin/__init__.py
+++ Frames/SubFrames/Admin/__init__.py
@@ -1,3 +1,2 @@
 from .mainA import admin_gui
 from .Class.createClass import ders_ekleme_sayfasi
-from .Class.classes import dersler_yonetim_penceresi
