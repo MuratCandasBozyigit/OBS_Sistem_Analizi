3493e04751ce7c1515f46f2fba1eeaba47b00e1c
Murti <bozyigit31@gmail.com>
2025-05-13 02:02:22 +0300

    f

--- DB/Migrations/Student/__init__.py
+++ DB/Migrations/Student/__init__.py
@@ -1,4 +1,4 @@
-from .create import create_ogrenciler_table
+from .create import create_ogrenciler_table,ogrenci_ekle
 from .delete import ogrenci_sil
 from .read import tum_ogrencileri_getir
 from .update import ogrenci_guncelle
