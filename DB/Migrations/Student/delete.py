1e1ef9054b869e49a70b5b35a8ac5b6bc43ac1cc
Murti <bozyigit31@gmail.com>
2025-05-14 01:54:23 +0300

    g

--- DB/Migrations/Student/delete.py
+++ DB/Migrations/Student/delete.py
@@ -1,8 +1,20 @@
 from DB.connection import get_connection
 
-def ogrenci_sil(ogrenci_id):
+# def ogrenci_sil(ogrenci_id):
+#     conn = get_connection()
+#     cursor = conn.cursor()
+#     cursor.execute("DELETE FROM ogrenciler WHERE ogrenci_id = ?", (ogrenci_id,))
+#     conn.commit()
+#     conn.close()
+
+
+def ogretmen_sil(ogrenci_id):
     conn = get_connection()
     cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM ogretmen_ogrenci WHERE ogrenci_id = ?", (ogrenci_id,))
+
     cursor.execute("DELETE FROM ogrenciler WHERE ogrenci_id = ?", (ogrenci_id,))
+
     conn.commit()
     conn.close()
