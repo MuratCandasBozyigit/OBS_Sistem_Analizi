81f6302373314a5a0214a5b21a46975ee429f031
Murti <bozyigit31@gmail.com>
2025-05-14 01:19:27 +0300

    Yeni Delete Metodu MANY TO MANY SIKINTI ÇIKMASIN DİYE FK DAN DOLAYI

--- DB/Migrations/Teacher/delete.py
+++ DB/Migrations/Teacher/delete.py
@@ -1,8 +1,21 @@
 from DB.connection import get_connection
 
+# def ogretmen_sil(ogretmen_id):
+#     conn = get_connection()
+#     cursor = conn.cursor()
+#     cursor.execute("DELETE FROM ogretmenler WHERE ogretmen_id = ?", (ogretmen_id,))
+#     conn.commit()
+#     conn.close()
+
 def ogretmen_sil(ogretmen_id):
     conn = get_connection()
     cursor = conn.cursor()
+
+    # Önce öğretmenin ders ilişkilerini sil
+    cursor.execute("DELETE FROM ogretmen_ders WHERE ogretmen_id = ?", (ogretmen_id,))
+
+    # Sonra öğretmenin kendisini sil
     cursor.execute("DELETE FROM ogretmenler WHERE ogretmen_id = ?", (ogretmen_id,))
+
     conn.commit()
     conn.close()
