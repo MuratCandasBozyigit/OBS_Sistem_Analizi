b5f014122694b53d5966d919272e21c6e4be2ec1
Murti <bozyigit31@gmail.com>
2025-05-14 20:07:39 +0300

    BİR YERLERE VARIYORUZ

--- Frames/SubFrames/Admin/Class/__init__.py
+++ Frames/SubFrames/Admin/Class/__init__.py
@@ -1,4 +1,4 @@
 from .createClass import ders_ekleme_sayfasi
 from .getClasses import dersleri_listele_gui
 from .assignClassToStudent import *
-from .assignTeacherAndStudentToClass  import *
+from .assignTeacherAndStudentToClass  import ders_ata_sayfasi
