4a2ac83c03079a61d80ba76ba829851330e822ab
Murti <bozyigit31@gmail.com>
2025-05-19 01:29:24 +0300

    Her şey tamam sadece şimdi ogrencyi silmek ve güncellemek kaldı ekleyip getireiblyoruz yeni gelen şu iki değeri getirmediğim için henüz  kllanamıyoeum b ir eyşey kalmadı

--- Frames/SubFrames/Admin/Student/getStudents.py
+++ Frames/SubFrames/Admin/Student/getStudents.py
@@ -1,17 +1,18 @@
-import customtkinter as ctk
+﻿import customtkinter as ctk
 from tkinter import messagebox
 from DB.Migrations.Student import tum_ogrencileri_getir, ogrenci_sil, ogrenci_guncelle
 from . import createStudent  # Öğrenci ekleme penceresi
 from . import assignClassToStudent
 from . import assignTeacherToStudent
+
 def ogrencileri_listele_gui():
     win = ctk.CTkToplevel()
     win.title("Öğrenci Yönetimi")
-    win.geometry("1350x650")
+    win.geometry("1450x650")
     win.lift()
     win.attributes('-topmost', True)
     win.after(200, lambda: win.attributes('-topmost', False))
-    
+
     title = ctk.CTkLabel(win, text="Tüm Öğrenciler", font=("Arial", 22, "bold"))
     title.pack(pady=10)
 
@@ -35,13 +36,13 @@ def ogrencileri_listele_gui():
     )
     ekle_btn.pack(side="left", padx=10)
 
-    scroll_frame = ctk.CTkScrollableFrame(win, width=1200, height=460)
+    scroll_frame = ctk.CTkScrollableFrame(win, width=1300, height=460)
     scroll_frame.pack(padx=20, pady=10, fill="both", expand=True)
 
-    headers = ["ID", "Adı", "Soyadı", "Telefon", "TCKN", "Numara", "Fotoğraf", "Adres", "Şifre", "Güncelle", "Sil", "Ders İşlemleri","Öğretmen İşlemleri"]
+    headers = ["ID", "Adı", "Soyadı", "Telefon", "TCKN", "Numara", "Fotoğraf", "Adres", "Vize", "Final", "Şifre", "Güncelle", "Sil", "Ders İşlemleri", "Öğretmen İşlemleri"]
     for col, header in enumerate(headers):
         ctk.CTkLabel(scroll_frame, text=header, font=("Arial", 15, "bold")).grid(
-            row=0, column=col, padx=15, pady=5, sticky="w"
+            row=0, column=col, padx=10, pady=5, sticky="w"
         )
 
     ogrenciler = tum_ogrencileri_getir()
@@ -63,12 +64,12 @@ def ogrencileri_listele_gui():
             except Exception as e:
                 messagebox.showerror("Hata", f"Silinemedi!\n{e}")
 
-    def guncelle_goster(row_idx, ogrenci_id, mevcut_ad, mevcut_soyad, mevcut_tel, mevcut_tckn, mevcut_numara, mevcut_foto, mevcut_adres, mevcut_sifre):
+    def guncelle_goster(row_idx, ogrenci_id, mevcut_ad, mevcut_soyad, mevcut_tel, mevcut_tckn, mevcut_numara, mevcut_foto, mevcut_adres, mevcut_vize, mevcut_final, mevcut_sifre):
         if guncelle_form["frame"]:
             guncelle_form["frame"].destroy()
 
         form = ctk.CTkFrame(scroll_frame, fg_color="#F0F0F0")
-        form.grid(row=row_idx+1, column=0, columnspan=12, pady=(5, 15), sticky="ew", padx=10)
+        form.grid(row=row_idx+1, column=0, columnspan=15, pady=(5, 15), sticky="ew", padx=10)
 
         def iptal_et():
             form.destroy()
@@ -88,38 +89,43 @@ def ogrencileri_listele_gui():
         entry_numara = add_label_entry(2, 1, "Öğrenci No:", mevcut_numara)
         entry_foto = add_label_entry(2, 3, "Fotoğraf URL:", mevcut_foto)
         entry_adres = add_label_entry(3, 1, "Adres:", mevcut_adres)
-        entry_sifre = add_label_entry(3, 3, "Şifre:", mevcut_sifre, show="*")
+        entry_vize = add_label_entry(3, 3, "Vize:", mevcut_vize)
+        entry_final = add_label_entry(4, 1, "Final:", mevcut_final)
+        entry_sifre = add_label_entry(4, 3, "Şifre:", mevcut_sifre, show="*")
 
         def kaydet():
-            yeni_ad = entry_ad.get().strip()
-            yeni_soyad = entry_soyad.get().strip()
-            yeni_tel = entry_tel.get().strip()
-            yeni_tckn = entry_tckn.get().strip()
-            yeni_numara = entry_numara.get().strip()
-            yeni_foto = entry_foto.get().strip()
-            yeni_adres = entry_adres.get().strip()
-            yeni_sifre = entry_sifre.get().strip()
-
-            if not all([yeni_ad, yeni_soyad, yeni_tel, yeni_tckn, yeni_numara, yeni_foto, yeni_adres, yeni_sifre]):
-                messagebox.showerror("Hata", "Alanlar boş bırakılamaz.")
-                return
-
             try:
-                ogrenci_guncelle(ogrenci_id, yeni_ad, yeni_soyad, yeni_foto, yeni_adres, yeni_tel, yeni_tckn, yeni_numara, yeni_sifre)
+                yeni_ad = entry_ad.get().strip()
+                yeni_soyad = entry_soyad.get().strip()
+                yeni_tel = entry_tel.get().strip()
+                yeni_tckn = entry_tckn.get().strip()
+                yeni_numara = entry_numara.get().strip()
+                yeni_foto = entry_foto.get().strip()
+                yeni_adres = entry_adres.get().strip()
+                yeni_vize = float(entry_vize.get().strip())
+                yeni_final = float(entry_final.get().strip())
+                yeni_sifre = entry_sifre.get().strip()
+
+                if not all([yeni_ad, yeni_soyad, yeni_tel, yeni_tckn, yeni_numara, yeni_foto, yeni_adres, yeni_sifre]):
+                    messagebox.showerror("Hata", "Alanlar boş bırakılamaz.")
+                    return
+
+                ogrenci_guncelle(ogrenci_id, yeni_ad, yeni_soyad, yeni_foto, yeni_adres, yeni_tel, yeni_tckn, yeni_numara, yeni_vize, yeni_final, yeni_sifre)
                 messagebox.showinfo("Başarılı", "Öğrenci güncellendi.")
                 refresh()
+
             except Exception as e:
                 messagebox.showerror("Hata", f"Güncelleme hatası:\n{e}")
 
-        ctk.CTkButton(form, text="Kaydet", command=kaydet, width=100, fg_color="#FFA500", hover_color="#FF8C00").grid(row=4, column=4, padx=10)
-        ctk.CTkButton(form, text="İptal", command=iptal_et, width=80, fg_color="gray", hover_color="darkgray").grid(row=4, column=5, padx=10, pady=5)
+        ctk.CTkButton(form, text="Kaydet", command=kaydet, width=100, fg_color="#FFA500", hover_color="#FF8C00").grid(row=5, column=4, padx=10)
+        ctk.CTkButton(form, text="İptal", command=iptal_et, width=80, fg_color="gray", hover_color="darkgray").grid(row=5, column=5, padx=10, pady=5)
 
         guncelle_form["frame"] = form
 
     for i, ogrenci in enumerate(ogrenciler, start=1):
-        ogrenci_id, ad, soyad, foto, adres, tel, tckn, numara, sifre = ogrenci
+        ogrenci_id, ad, soyad, foto, adres, tel, tckn, numara, vize, final, sifre = ogrenci
 
-        for idx, val in enumerate([ogrenci_id, ad, soyad, tel, tckn, numara, foto, adres, sifre]):
+        for idx, val in enumerate([ogrenci_id, ad, soyad, tel, tckn, numara, foto, adres, vize, final, sifre]):
             ctk.CTkLabel(scroll_frame, text=str(val), font=("Arial", 12)).grid(
                 row=i, column=idx, padx=5, pady=5, sticky="w"
             )
@@ -127,23 +133,21 @@ def ogrencileri_listele_gui():
         ctk.CTkButton(
             scroll_frame, text="Güncelle", font=("Arial", 12), fg_color="#FFA500", hover_color="#FF8C00",
             command=lambda row=i, oid=ogrenci_id: guncelle_goster(
-                row, oid, ad, soyad, tel, tckn, numara, foto, adres, sifre
+                row, oid, ad, soyad, tel, tckn, numara, foto, adres, vize, final, sifre
             )
-        ).grid(row=i, column=9, padx=5, pady=5)
+        ).grid(row=i, column=11, padx=5, pady=5)
 
         ctk.CTkButton(
             scroll_frame, text="Sil", font=("Arial", 12), fg_color="#FF6347", hover_color="#FF4500",
             command=lambda oid=ogrenci_id: sil_ogrenci(oid)
-        ).grid(row=i, column=10, padx=5, pady=5)
+        ).grid(row=i, column=12, padx=5, pady=5)
 
         ctk.CTkButton(
             scroll_frame, text="Ders İşlemleri", font=("Arial", 12), fg_color="#4682B4", hover_color="#4169E1",
             command=lambda oid=ogrenci_id: assignClassToStudent.ders_ekle_ogrenci(oid, scroll_frame)
-        ).grid(row=i, column=11, padx=5, pady=5)
+        ).grid(row=i, column=13, padx=5, pady=5)
 
         ctk.CTkButton(
             scroll_frame, text="Öğretmen İşlemleri", font=("Arial", 12), fg_color="#4682B4", hover_color="#4169E1",
             command=lambda oid=ogrenci_id: assignTeacherToStudent.ogretmen_ekle_ogrenci(oid, scroll_frame)
-        ).grid(row=i, column=12, padx=5, pady=5)
-
-
+        ).grid(row=i, column=14, padx=5, pady=5)
