412ce3fa59f6b478dbf989125fe64c423ec429d5
Murti <bozyigit31@gmail.com>
2025-05-18 14:08:33 +0300

    Yeni Db Mantığı ve yeni çalışma stlili  yeni mycalsses yeni yapı

--- DB/Migrations/Student/create.py
+++ DB/Migrations/Student/create.py
@@ -1,5 +1,6 @@
 from DB.connection import get_connection
 
+
 def create_ogrenciler_table():
     conn = get_connection()
     cursor = conn.cursor()
@@ -13,17 +14,18 @@ def create_ogrenciler_table():
             ogrenci_tel_no TEXT,
             ogrenci_tckn TEXT NOT NULL,
             ogrenci_numarası TEXT NOT NULL,
-            sifre TEXT NOT NULL
+            sifre TEXT NOT NULL,
+            ogrenci_vize REAL,
+            ogrenci_final REAL
         )
     ''')
     conn.commit()
     conn.close()
 
-
-def ogrenci_ekle(ad, soyad, fotoğraf, adres, tel_no, tckn, ogr_no, sifre):
+def ogrenci_ekle(ad, soyad, fotoğraf, adres, tel_no, tckn, ogr_no, sifre, vize=None, final=None):
     conn = get_connection()
     cursor = conn.cursor()
-    
+
     cursor.execute("""
         INSERT INTO ogrenciler (
             ogrenci_adı,
@@ -33,9 +35,11 @@ def ogrenci_ekle(ad, soyad, fotoğraf, adres, tel_no, tckn, ogr_no, sifre):
             ogrenci_tel_no,
             ogrenci_tckn,
             ogrenci_numarası,
-            sifre
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    """, (ad, soyad, fotoğraf, adres, tel_no, tckn, ogr_no, sifre))
+            sifre,
+            ogrenci_vize,
+            ogrenci_final
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (ad, soyad, fotoğraf, adres, tel_no, tckn, ogr_no, sifre, vize, final))
 
     conn.commit()
     conn.close()
