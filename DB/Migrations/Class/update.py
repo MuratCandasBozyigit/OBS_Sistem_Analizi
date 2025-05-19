68c78cb91a0b45ccf373b0b0575824a4e049ad04
Murti <bozyigit31@gmail.com>
2025-05-19 21:44:34 +0300

    DERS TAMAMEN BİTTİ

--- DB/Migrations/Class/update.py
+++ DB/Migrations/Class/update.py
@@ -1,6 +1,16 @@
 from DB.connection import get_connection
 
 def ders_guncelle(ders_id, yeni_ders_adi, yeni_ders_saati):
+    # ---------- VALIDASYON ----------
+    try:
+        yeni_ders_saati = int(yeni_ders_saati)  # Saati int'e çevir
+    except ValueError:
+        raise ValueError("Ders saati sayısal olmalıdır.")
+    
+    if not (1 <= yeni_ders_saati <= 30):
+        raise ValueError("Ders saati 1 ile 30 arasında olmalıdır.")
+    
+    # ---------- GÜNCELLEME ----------
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
